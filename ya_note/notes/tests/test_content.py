1e5aa68d6b8183c824ea2b6e0ed0d8f77b7b58fb
RavenIV <torrans5@yandex.ru>
2023-12-09 17:55:03 +0300

    Update test_content for ya_notes

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,35 +1,32 @@
 from notes.forms import NoteForm
-from notes.tests import utils
+from notes.models import Note
+from notes.tests.utils import (
+    TestBase, URL_NOTE_ADD, URL_NOTE_EDIT, URL_NOTES_LIST
+)
 
 
-class TestNotesListPage(utils.TestBase):
+class TestNotesListPage(TestBase):
 
     def test_notes_list_for_author(self):
-        note_title = self.note.title
-        note_text = self.note.text
-        note_slug = self.note.slug
-        note_author = self.note.author
-        self.assertIn(
-            self.note,
-            self.author_client.get(utils.URL_NOTES_LIST).context['object_list']
+        notes = self.author_client.get(URL_NOTES_LIST).context['object_list']
+        self.assertIn(self.note, notes)
+        self.assertEqual(
+            len([note.id for note in notes if note.id == self.note.id]), 1
         )
-        self.assertEqual(self.note.title, note_title)
-        self.assertEqual(self.note.text, note_text)
-        self.assertEqual(self.note.slug, note_slug)
-        self.assertEqual(self.note.author, note_author)
+        note_from_db = Note.objects.get(id=self.note.id)
+        self.assertEqual(note_from_db.title, self.note.title)
+        self.assertEqual(note_from_db.text, self.note.text)
+        self.assertEqual(note_from_db.slug, self.note.slug)
+        self.assertEqual(note_from_db.author, self.note.author)
 
     def test_notes_list_for_another_user(self):
         self.assertNotIn(
             self.note,
-            self.user_client.get(utils.URL_NOTES_LIST).context['object_list']
+            self.user_client.get(URL_NOTES_LIST).context['object_list']
         )
 
     def test_notes_pages_contain_form(self):
-        for url in (
-            utils.URL_NOTE_ADD,
-            utils.URL_NOTE_EDIT
-        ):
+        for url in (URL_NOTE_ADD, URL_NOTE_EDIT):
             with self.subTest(url=url):
                 response = self.author_client.get(url)
-                self.assertIn('form', response.context)
-                self.assertIsInstance(response.context['form'], NoteForm)
+                self.assertIsInstance(response.context.get('form'), NoteForm)

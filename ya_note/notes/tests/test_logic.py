99d090eff493f9daeebaf8e7cfa5ebe52f23efbc
RavenIV <torrans5@yandex.ru>
2023-12-10 14:06:59 +0300

    Update ya_note: test_logic

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -15,16 +15,15 @@ from notes.tests.utils import (
 
 class TestNoteCreation(TestBase):
 
-    def add_note_successfully(self):
-        ids_before = {id[0] for id in Note.objects.values_list('id')}
+    def check_note_creation(self):
+        notes = set(Note.objects.all())
         self.assertRedirects(
             self.user_client.post(URL_NOTE_ADD, self.form_data),
             URL_SUCCESS
         )
-        ids_after = {id[0] for id in Note.objects.values_list('id')}
-        new_note_id = ids_after.difference(ids_before)
-        self.assertEqual(len(new_note_id), 1)
-        new_note = Note.objects.get(id=new_note_id.pop())
+        notes = set(Note.objects.all()) - notes
+        self.assertEqual(len(notes), 1)
+        new_note = notes.pop()
         self.assertEqual(new_note.text, self.form_data['text'])
         self.assertEqual(new_note.title, self.form_data['title'])
         self.assertEqual(new_note.author, self.user)
@@ -37,40 +36,27 @@ class TestNoteCreation(TestBase):
         )
 
     def test_anonymous_user_cant_create_note(self):
-        notes = list(Note.objects.values_list(
-            'id', 'title', 'text', 'slug', 'author'
-        ))
+        notes = set(Note.objects.all())
         self.client.post(URL_NOTE_ADD, self.form_data)
-        self.assertEqual(
-            notes,
-            list(Note.objects.values_list(
-                'id', 'title', 'text', 'slug', 'author'
-            ))
-        )
+        self.assertEqual(notes, set(Note.objects.all()))
 
     def test_authenticated_user_can_create_note(self):
-        self.add_note_successfully()
+        self.check_note_creation()
 
     def test_user_cant_use_existing_slug(self):
         self.form_data['slug'] = self.note.slug
+        notes = set(Note.objects.all())
         self.assertFormError(
             self.user_client.post(URL_NOTE_ADD, self.form_data),
             form='form',
             field='slug',
             errors=self.form_data['slug'] + WARNING
         )
-        self.assertEqual(
-            Note.objects.filter(slug=self.form_data['slug']).count(), 1
-        )
-        note_from_db = Note.objects.get(id=self.note.id)
-        self.assertEqual(note_from_db.title, self.note.title)
-        self.assertEqual(note_from_db.text, self.note.text)
-        self.assertEqual(note_from_db.slug, self.note.slug)
-        self.assertEqual(note_from_db.author, self.note.author)
+        self.assertEqual(notes, set(Note.objects.all()))
 
     def test_empty_slug(self):
         self.form_data.pop('slug')
-        self.add_note_successfully()
+        self.check_note_creation()
 
     def test_author_can_delete_note(self):
         notes_count = Note.objects.count()
@@ -78,17 +64,18 @@ class TestNoteCreation(TestBase):
             self.author_client.delete(URL_NOTE_DELETE),
             URL_SUCCESS
         )
-        self.assertEqual(notes_count - 1, Note.objects.count())
-        with self.assertRaises(Note.DoesNotExist):
-            Note.objects.get(id=self.note.id)
+        notes = Note.objects.all()
+        self.assertEqual(notes_count - 1, notes.count())
+        self.assertFalse(notes.filter(id=self.note.id).exists())
 
     def test_user_cant_delete_note_of_another_user(self):
         self.assertEqual(
             self.user_client.delete(URL_NOTE_DELETE).status_code,
             HTTPStatus.NOT_FOUND
         )
-        self.assertIn(self.note, Note.objects.all())
-        note_from_db = Note.objects.get(id=self.note.id)
+        notes = Note.objects.filter(id=self.note.id)
+        self.assertTrue(notes.exists())
+        note_from_db = notes.get(id=self.note.id)
         self.assertEqual(note_from_db.text, self.note.text)
         self.assertEqual(note_from_db.title, self.note.title)
         self.assertEqual(note_from_db.slug, self.note.slug)
